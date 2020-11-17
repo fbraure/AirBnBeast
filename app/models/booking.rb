c4b28067d420523c14d18d7250d495d67a8afa44
Florent BRAURE <florent.braure@gmail.com>
2020-11-17 12:33:07 +0100

    Add Booking MVC

---
+++ app/models/booking.rb
@@ -0,0 +1,7 @@
+class Booking < ApplicationRecord
+  belongs_to :user
+  belongs_to :offer
+
+  # 1 : liaison active, 0:  réservation annulée
+  validates :status, presence: true, numericality: { in: [0, 1] }
+end
