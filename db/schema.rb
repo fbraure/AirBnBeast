a05285ccae667941008c1835359a5f6353a5937d
JeanChristophe Sabatier <sabatier.jeanchristophe@gmail.com>
2020-11-17 10:44:03 +0100

    Creating MVC Offer

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,15 @@ ActiveRecord::Schema.define(version: 2020_11_17_075107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "offers", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "price"
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
