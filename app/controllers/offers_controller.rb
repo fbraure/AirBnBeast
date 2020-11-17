0bc9cd227c8afcd0686f4431a96d9e47952c24f5
Charlotte Martineau <charlotte_martineau@hotmail.fr>
2020-11-17 16:34:41 +0100

    add method mine in offers_controller and add mine in views/offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -7,4 +7,10 @@ class OffersController < ApplicationController
   def search
 
   end
+
+  def mine
+    # Trouver les offres que le vendeur a cree pour les afficher
+    @offers = Offer.where(user: current_user)
+    # Trouver les offres que user a achete pour les afficher
+  end
 end
