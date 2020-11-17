b345f176011e673aa81d8709803e90ae36c4fc7c
Florent BRAURE <florent.braure@gmail.com>
2020-11-17 23:55:50 +0100

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,35 @@
 require 'faker'
 puts "SEED: START CLEAR"
-Offer.destroy_all
-Review.destroy_all
-User.destroy_all
 Booking.destroy_all
+Review.destroy_all
+Offer.destroy_all
+User.destroy_all
 puts "SEED: START POPULATE USERS"
 seller1 = User.create!(
+  first_name: "Patrick",
+  last_name: "ABIDBOL",
   email: 'seller1@gmail.com',
   password: 'seller01@',
+  description: "It's raining men ,alleluia !",
+  avatar_url: "https://kitt.lewagon.com/placeholder/users/random",
   is_seller: true
 )
 seller2 = User.create!(
+  first_name: "John F.",
+  last_name: "KENNEDY",
   email: 'seller2@gmail.com',
   password: 'seller02@',
+  description: "What else...",
+  avatar_url: "https://kitt.lewagon.com/placeholder/users/random",
   is_seller: true
 )
 customer1 = User.create!(
+  first_name: "La Petite",
+  last_name: "SIRENE",
+  description: "Sur la plage abandonnée... Coquillages et crustacés !",
   email: 'customer1@gmail.com',
   password: 'customer01@',
+  avatar_url: "https://kitt.lewagon.com/placeholder/users/random",
   is_seller: false
 )
 puts "SEED: START POPULATE OFFERS"
@@ -32,17 +44,24 @@ puts "SEED: START POPULATE OFFERS"
     offer.save!
   end
 puts "SEED: START POPULATE BOOKINGS"
+# Une offre n'apparraitra pas deux fois
+Offer.first(12).shuffle.each do |offer|
+  booking = Booking.new(
+    status: [0, 1, 1, 1],
+    user: User.all.sample,
+    offer: offer
+  )
+end
 puts "SEED: END POPULATE"
 
-# secret seed
-return if false
+# secret seed (false => true pour activer)
+return unless ENV["secret"] || false
 
 wagonners = [
   ["violaine", "https://avatars1.githubusercontent.com/u/70322815?v=4"],
   ["anne-cecile", "https://avatars3.githubusercontent.com/u/34253262?v=4"],
   ["charlotte", "https://avatars2.githubusercontent.com/u/70255612?v=4"],
   ["matthias", "https://avatars2.githubusercontent.com/u/48900320?v=4"],
-  ["Titouan", "https://avatars3.githubusercontent.com/u/71375669?v=4"],
   ["Florent", "https://avatars2.githubusercontent.com/u/65367849?v=4"],
   ["Mael", "https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1601908758/rtos8nfz2zacii2dndkc.jpg"],
   ["Baptiste", "https://avatars3.githubusercontent.com/u/66240536?v=4"],
@@ -89,6 +108,29 @@ nicknames = [
   ["dit Le Muffle", true],
   ["Dingoss", false]
 ]
+descriptions = [
+  "Aussi agile avec un couteau, qu'une trotinette",
+  "Des chefs d'états ont déjà eu recourt à mes services. Certains sont encore vivant",
+  "Je suis l'élite des roublards des bas fonds",
+  "Certains m'ont déjà croisé marchant dans la rue, nu en lisant Le Figaro. C'est ça la vie pour moi",
+  "Peu le savent, mais un tatouage représentant un pénis dansant le flamenco est caché sur mon pied",
+  "J'ai horreur de tous les fruits et légumes bicolores. Même en automne",
+  "Rien de particulier à dire. J'ai faim...",
+  "Outre mon air de Sean Connery, appréhende le vice et la perversité comme personne",
+  "Mon apparence me fait défaut, mais ma cruauté compense !",
+  "Mes passions dans la vie : Jouer à la roue de la fortune les yeux bandés et faire des ballades en poney, les jours de pluie",
+  "Je mérite à être connue. Sauf par la police...",
+  "Je peux faire les pires crasses pour un peu d'argent",
+  "And so whaaaaaaaat ?",
+  "Juste moi, what else ?",
+  "Bip Bap A Loolaaaaa",
+  "J'aime bien espionner les gens qui dorment dans les canapés du Wagon",
+  "J'ai déjà craché dans une tasse du Wagon avant de la reposer :)",
+  "Pas de scrupule à faire souffrire les gens. La vie est une chienne",
+  "On peut pas décrire mille fois mille personnes, mais... non... ",
+  "On m'appele aussi Elwis Presse Les. Je sais pas pourquoi.",
+  "VOTEZ POUR MOI"
+]
 pranks = [
   ["Epiler la victime au Chartertone", "Une souffrance jamais égalée"],
   ["Mettre une punaise sur la chaise", "Une attaque directe à la fierté de l'adversaire"],
@@ -97,6 +139,7 @@ pranks = [
   ["Emietter trois Granolas dans le lit de la victime", "Nombreux sont ceux qui ne s'en sont jamais remis"],
   ["Assommer avec un curcurbitacé", "Tuer en s'amusant"],
   ["Déposer une couche sale dans la chambre de la victime endormie", "Trés peu pour moi"],
+  ["La fameuse crotte de chien dans un sac enflammé", "Ne marche pas dans le Sud. Il n'ont pas de chaussures"],
   ["Ligoter et forcer à manger 127 nuggets Mc Do", "En l'écrivant j'ai vomi dans ma bouche"],
   ["Uriner dans les chaussures de la victime", "Simple, mais toujours efficace. Et salutaire"],
   ["Giffler", "Un classique. Rechauffe les mains en hiver"],
@@ -105,30 +148,53 @@ pranks = [
   ["Refiler des morpions", "Encore faut-il en avoir"],
   ["Assassinat en découpant la carotide avec une feuille de chêne", "Le tarif dépend de la saison"],
   ["Biffler", "No Comment"],
+  ["Une decapitation standard", "No Comment"],
   ["Cracher dans le café de la victime", "Après une pizza de chez Marco"],
   ["Jeter des orties et partir en courant", "Le prix reflète l'effort fourni"],
+  ["Poser un piège à loups, sur la tête de la victime", "Penser à porter une blouse"],
+  ["Mettre de la superglue sur la cuvette des toilettes", "Pire chez les victimes velues"],
+  ["Jeter un pot de peinture indélébile sur la victime", "Ne pas hésiter à demander le suppléments Plumes et Graviers"],
   ["Poil à gratter", "Favoriser le poil de loutre. Ou de phoque"]
 ]
 
 puts "SECRET SEED: START CLEAR"
-Offer.destroy_all
-Review.destroy_all
-User.destroy_all
 Booking.destroy_all
+Review.destroy_all
+Offer.destroy_all
+User.destroy_all
 puts "SECRET SEED: START POPULATE USERS"
+#login toujours present pour test
+wagonner = ["Titouan", "https://avatars3.githubusercontent.com/u/71375669?v=4"]
+nickname = nicknames.sample
+description = descriptions.sample
+User.create!(
+    first_name: nickname[1] ? wagonner[0].capitalize : nickname[0].capitalize,
+    last_name: nickname[1] ? nickname[0].capitalize : wagonner[0].capitalize,
+    email: "#{wagonner[0].strip.downcase}@test.fr",
+    password: "wagonner",
+    avatar_url: wagonner[1],
+    description: description
+    # is_seller: true
+  )
 18.times do
   wagonner = wagonners.sample
   nickname = nicknames.sample
+  description = descriptions.sample
   User.create!(
     first_name: nickname[1] ? wagonner[0].capitalize : nickname[0].capitalize,
     last_name: nickname[1] ? nickname[0].capitalize : wagonner[0].capitalize,
     email: "#{wagonner[0].strip.downcase}@test.fr",
     password: "wagonner",
-    is_seller: true
+    avatar_url: wagonner[1],
+    description: description
+    # is_seller: true
   )
-  wagonners.delete(wagonner) # Pour éviter 2x le meme user
+   # Pour éviter 2x les doublons
+  wagonners.delete(wagonner)
+  descriptions.delete(description)
 end
 puts "SECRET SEED: START POPULATE OFFERS"
+users = User.all
   24.times do
     prank = pranks.sample;
     Offer.create!(
@@ -136,15 +202,24 @@ puts "SECRET SEED: START POPULATE OFFERS"
       description: prank[1],
       price: Faker::Number.number(digits: 3),
       date: Faker::Date.between(from: 30.days.ago, to: 30.days.from_now),
-      user: User.all.sample
+      user: users.sample
     )
   end
 puts "SECRET SEED: START POPULATE BOOKINGS"
-30.times do
-  Booking.create!(
-    status: [0, 1, 1].sample,
-    user: User.all.sample,
-    offer: Offer.all.sample,
+Offer.all.first(18).shuffle.each do |offer|
+  booking = Booking.new(
+    status: [0, 1, 1, 1].sample,
+    user: users.sample,
+    offer: offer
   )
+  if booking.save!
+    unless booking.status
+      Booking.new(
+        status: 1,
+        user: users.sample,
+        offer: offer
+      )
+    end
+  end
 end
 puts "SECRET SEED: END POPULATE"
