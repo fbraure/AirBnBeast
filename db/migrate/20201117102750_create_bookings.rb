536b435c4a7e6b3070fa915cf270abb7428d22f7
Flo <65367849+fbraure@users.noreply.github.com>
2020-11-17 14:12:06 +0100

    Update 20201117102750_create_bookings.rb

--- db/migrate/20201117102750_create_bookings.rb
+++ db/migrate/20201117102750_create_bookings.rb
@@ -1,7 +1,7 @@
 class CreateBookings < ActiveRecord::Migration[6.0]
   def change
     create_table :bookings do |t|
-      t.integer :status
+      t.integer :status, default: 1
       t.references :user, null: false, foreign_key: true
       t.references :offer, null: false, foreign_key: true
 
