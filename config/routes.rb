2c7945be39f2ec06f86a1a9dfd3b1b101191af54
JeanChristophe Sabatier <sabatier.jeanchristophe@gmail.com>
2020-11-17 19:19:50 +0100

    Offers index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  resources :offers, only:[ :index, :search ]
+
   resources :offers do
     resources :reviews, only: [ :new, :create ]
   end
