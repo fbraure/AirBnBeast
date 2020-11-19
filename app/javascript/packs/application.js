26834096e49ced122ac249d82a9714e91999e1a5
Florent BRAURE <florent.braure@gmail.com>
2020-11-19 11:48:39 +0100

    test non fructueux

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,10 +25,9 @@ require("channels")
 // External imports
 import "bootstrap";
 
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+// Internal imports
+import {addSoundsOnClick} from "../components/sounded_button.js";
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  addSoundsOnClick();
 });
